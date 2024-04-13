fabe4e7f0d7399f96624f852fe3afccf46442192
khaledmaher2001 <khaled.maher709@gmail.com>
2024-04-13 21:34:37 +0200

    khaled 13/4

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,30 @@
 import 'dart:developer';
 import 'dart:io';
- import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+import 'package:task_2_admin/firebase_options.dart';
 import 'package:task_2_admin/home/cubit/cubit.dart';
-
 import 'home/home_screen.dart';
-
-
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isIOS) {
-    await Firebase.initializeApp();
-  } else if(Platform.isAndroid) {
-    await Firebase.initializeApp();
-  }
-  log("0"*30);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  log("0" * 30);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-
-        BlocProvider(create: (context) => HomeCubit() ),
-
-
+        BlocProvider(create: (context) => HomeCubit()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
